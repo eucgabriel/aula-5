1327d932c02f3189a8070da90e0dd7cce1e7339f
Carlos Gabriel <canalcarlosgabriel1407@gmail.com>
2022-02-01 17:39:35 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CG</title>
+</head>
+<body>
+    <?php
+        $n1 = 2;
+        $n2 = 3;
+        echo "$n1 + $n2 = ".($n1 + $n2);
+        echo "<br>$n1 - $n2 = ".($n1 + $n2);
+        echo "<br>$n1 * $n2 = ".($n1 + $n2);
+        echo "<br>$n1 / $n2 = ".($n1 + $n2);
+        echo "<br>O módulo vale ".($n1 + $n2);
+    ?>
+</body>
+</html>
