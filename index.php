e15e0b16d1c7f96e258eede83485a4fbd32f4c0e
Carlos Gabriel <canalcarlosgabriel1407@gmail.com>
2022-02-06 15:25:32 -0300

    fundo + css

--- index.php
+++ index.php
@@ -5,16 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CG</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-        $n1 = 2;
-        $n2 = 3;
-        echo "$n1 + $n2 = ".($n1 + $n2);
-        echo "<br>$n1 - $n2 = ".($n1 + $n2);
-        echo "<br>$n1 * $n2 = ".($n1 + $n2);
-        echo "<br>$n1 / $n2 = ".($n1 + $n2);
-        echo "<br>O módulo vale ".($n1 + $n2);
-    ?>
+    <div>
+        <?php
+            $n1 = 2;
+            $n2 = 3;
+            echo "$n1 + $n2 = ".($n1 + $n2);
+            echo "<br>$n1 - $n2 = ".($n1 + $n2);
+            echo "<br>$n1 * $n2 = ".($n1 + $n2);
+            echo "<br>$n1 / $n2 = ".($n1 + $n2);
+            echo "<br>O módulo vale ".($n1 + $n2);
+        ?>
+    </div>
 </body>
 </html>
