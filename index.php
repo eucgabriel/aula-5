6008ac2dec0aa6d0af1bee4897a61bb04a0048b1
Carlos Gabriel <canalcarlosgabriel1407@gmail.com>
2022-02-06 16:03:07 -0300

    final

--- index.php
+++ index.php
@@ -10,13 +10,21 @@
 <body>
     <div>
         <?php
-            $n1 = 2;
-            $n2 = 3;
+            $n1 = $_GET["a"];
+            $n2 = $_GET["b"];
+            echo "<h2>OS VALORES SÃO $n1 E $n2</h2>";
+            $m  = ($n1 + $n2) / 2;
             echo "$n1 + $n2 = ".($n1 + $n2);
-            echo "<br>$n1 - $n2 = ".($n1 + $n2);
-            echo "<br>$n1 * $n2 = ".($n1 + $n2);
-            echo "<br>$n1 / $n2 = ".($n1 + $n2);
-            echo "<br>O módulo vale ".($n1 + $n2);
+            echo "<br>$n1 - $n2 = ".($n1 - $n2);
+            echo "<br>$n1 * $n2 = ".($n1 * $n2);
+            echo "<br>$n1 / $n2 = ".($n1 / $n2);
+            echo "<br>$n1<sup>$n2</sup> =".pow($n1, $n2);
+            echo "<br> Média = $m";
+            echo "<br>O valor Absoluto de $n1 e $n2 = ".abs($n1)." e ".abs($n2);
+            echo "<br>A raiz de $n1 e $n2 é ".sqrt($n1)." e ".sqrt($n2);
+            echo "<br>O valor arredondado de $n1 e $n2 é ".round($n1)." e ".round($n2);
+            echo "<br>A parte inteira do nº $n1 e $n2 é ".intval($n1)." e ".intval($n2);
+            echo "<br>O valor de $n1 e $n2 em moeda é R$".number_format($n1, 2, ",", ".")." e R$".number_format($n2, 2, ",", ".");
         ?>
     </div>
 </body>
